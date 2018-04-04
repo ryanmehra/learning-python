5cb6d6a807a8b356af8d86ef757c98ea044b45cf
ryanmehra <37853653+ryanmehra@users.noreply.github.com>
2018-04-03 20:19:40 -0400

    Add files via upload

---
+++ TakeABreak.py
@@ -0,0 +1,11 @@
+# In this project after a given amount of time a webbrower
+# will open
+
+# Importing commands
+import time
+import webbrowser
+# how many seconds to wait
+time.sleep(10)
+# web browser opening command (choose your browser in between
+# the parentheses )
+webbrowser.open("https://www.youtube.com")
